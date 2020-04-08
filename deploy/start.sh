ad0cd1f22df2e54dfe27893954168b55bfc67f66
huaairen <huaairen@sina.com>
2020-04-08 22:18:19 +0800

    添加部署相关内容。

---
+++ deploy/start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+nohup java -jar kuaidi.jar  >kuaidi.log &
---
+++ deploy/stop.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PID=$(ps -ef | grep kuaidi.jar | grep -v grep | awk '{ print $2 }')
+if [ -z "$PID" ]
+then
+    echo Application is already stopped
+else
+    echo kill $PID
+    kill $PID
+fi
