4b9a447e3eaa2f0145cb179ce3872b693ad89966
huaairen <huaairen@sina.com>
2020-04-09 20:03:00 +0800

    删除deploy包。

--- deploy/start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-nohup java -jar kuaidi.jar  >kuaidi.log &
--- deploy/stop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-PID=$(ps -ef | grep kuaidi.jar | grep -v grep | awk '{ print $2 }')
-if [ -z "$PID" ]
-then
-    echo Application is already stopped
-else
-    echo kill $PID
-    kill $PID
-fi
